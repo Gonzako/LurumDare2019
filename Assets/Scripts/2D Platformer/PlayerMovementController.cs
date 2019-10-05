621dcfa9d014eac600c3bb3615cbfa2812256b05
Mike <miguelito_94_yo@hotmail.com>
2019-10-05 11:06:33 +0200

    Ink Painter importado Movimiento Básico (Con salto de paredes y posibilidad de plataformas móviles)

--- Assets/Scripts/2D Platformer/PlayerMovementController.cs
+++ Assets/Scripts/2D Platformer/PlayerMovementController.cs
@@ -30,6 +30,7 @@ public class PlayerMovementController : MonoBehaviour, IMove
     //button checks in update to see if we pressed the button during the last frame
     private void Update()
     {
+
         if (Input.GetButtonDown("Jump") && characterGrounding.IsGrounded)
         {
             rigidbody2D.AddForce(Vector2.up * jumpForce);
@@ -43,14 +44,12 @@ public class PlayerMovementController : MonoBehaviour, IMove
 
     private void FixedUpdate()
     {
-
         float horizontal = Input.GetAxis("Horizontal");
         Speed = horizontal;
 
         Vector3 movement = new Vector3(horizontal, 0);
 
         transform.position += movement * Time.deltaTime * moveSpeed;
-
     }
 
     internal void Bounce()
