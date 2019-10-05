eb4a9113a69bc4713f5a45c0844810276d343b73
Gonzako <al341522@uji.es>
2019-10-05 17:30:30 +0200

    added support for ontrigger stay points

--- Assets/Scripts/PaintExtentions/PaintAroundPlayer.cs
+++ Assets/Scripts/PaintExtentions/PaintAroundPlayer.cs
@@ -24,19 +24,23 @@ public class PaintAroundPlayer : MonoBehaviour
     [SerializeField]
     bool erase;
 
-    
-    private void OnTriggerEnter2D(Collider2D other)
+
+    private Vector3 getPointOfContact(Collider2D otherCol)
     {
-        Debug.Log("me cago en tó");
+        RaycastHit2D _hit = Physics2D.Raycast(transform.position, transform.position - otherCol.transform.position);
+        return _hit.point;
     }
+
+    #region UnityAPI
 
     private void Update()
     {
-        brush.RotateAngle += 40*Time.deltaTime;
+        brush.RotateAngle += 40 * Time.deltaTime;
         if (brush.RotateAngle > 360)
             brush.RotateAngle = 0;
     }
 
+   
     private void OnTriggerStay2D(Collider2D other)
     {
 
@@ -44,34 +48,35 @@ public class PaintAroundPlayer : MonoBehaviour
         bool success = true;
         var stuff = other.transform.GetComponent<InkCanvas>();
         var data = other.transform.GetComponent<PaintableObjectData>();
+        var pointOfContact = getPointOfContact(other);
+        if (stuff != null)
 
-        if (stuff != null && !data.IsPainted)
-            
-                switch (useMethodType)
-                {
-                    case UseMethodType.RaycastHitInfo:
-                        //success = erase ? paintObject.Erase(brush, hitInfo) : paintObject.Paint(brush, hitInfo);
-                        break;
-
-                    case UseMethodType.WorldPoint:
-                        success = erase ? stuff.Erase(brush, other.transform.position) : stuff.Paint(brush, other.transform.position+offSet);
-                        data.IsPainted = true;
+            switch (useMethodType)
+            {
+                case UseMethodType.RaycastHitInfo:
+                    //success = erase ? paintObject.Erase(brush, hitInfo) : paintObject.Paint(brush, hitInfo);
                     break;
 
-                    case UseMethodType.NearestSurfacePoint:
-                        //success = erase ? paintObject.EraseNearestTriangleSurface(brush, hitInfo.point) : paintObject.PaintNearestTriangleSurface(brush, hitInfo.point);
-                        break;
+                case UseMethodType.WorldPoint:
+                    success = erase ? stuff.Erase(brush, pointOfContact) : stuff.Paint(brush, pointOfContact);
+                    data.IsPainted = true;
+                    break;
 
-                    case UseMethodType.DirectUV:
-                        /*if (!(hitInfo.collider is MeshCollider))
-                            Debug.LogWarning("Raycast may be unexpected if you do not use MeshCollider.");
-                        success = erase ? paintObject.EraseUVDirect(brush, hitInfo.textureCoord) : paintObject.PaintUVDirect(brush, hitInfo.textureCoord);
-                        */
-                        break;
-                }
-            
-            if (!success)
-                Debug.LogError("Failed to paint.");
-    }
+                case UseMethodType.NearestSurfacePoint:
+                    //success = erase ? paintObject.EraseNearestTriangleSurface(brush, hitInfo.point) : paintObject.PaintNearestTriangleSurface(brush, hitInfo.point);
+                    break;
+
+                case UseMethodType.DirectUV:
+                    /*if (!(hitInfo.collider is MeshCollider))
+                        Debug.LogWarning("Raycast may be unexpected if you do not use MeshCollider.");
+                    success = erase ? paintObject.EraseUVDirect(brush, hitInfo.textureCoord) : paintObject.PaintUVDirect(brush, hitInfo.textureCoord);
+                    */
+                    break;
+            }
+
+        if (!success)
+            Debug.LogError("Failed to paint.");
+    } 
+    #endregion
 }
 
