339a34f05528c1f32579f1f53740ba90c58b0dd8
Mike <miguelito_94_yo@hotmail.com>
2019-10-06 12:20:28 +0200

    Enemies can now change colors

---
+++ Assets/Scripts/ColorChanging/Editor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(HueShifter))]
+class ColorCyclerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Cycle"))
+        {
+            HueShifter cycler = (HueShifter)target;
+            cycler.Cycle();
+        }
+    }
+}
