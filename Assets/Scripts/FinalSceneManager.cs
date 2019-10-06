2a1caa74b1ddfe32be2f79b1ddf597aebead6229
breynartstudios <breynartstudios@gmail.com>
2019-10-06 19:48:23 +0200

    Final Scene added

---
+++ Assets/Scripts/FinalSceneManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinalSceneManager : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    public Text txtMessage;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        TextValue();
+    }
+
+    public void GoToMainMenu()
+    {
+        StartCoroutine(ButtonSound("mainMenu"));
+    }
+
+    public void RestartGame()
+    {
+        StartCoroutine(ButtonSound("restartGame"));
+    }
+
+    public void TextValue()
+    {
+        if (CONST.playerWon)
+        {
+            txtMessage.text = "Congrats!\nYou won!";
+        }
+        else
+        {
+            txtMessage.text = "Sorry . . .\nYou lost!";
+        }
+    }
+
+    public void ChangeSFXValue()
+    {
+        StartCoroutine(ButtonSound("changeSFX"));
+    }
+
+    IEnumerator ButtonSound(string obj)
+    {
+        if (CONST.isSoundEnabled) audioSource.Play();
+        yield return new WaitForSeconds(0.2f);
+        if (obj == "mainMenu")
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (obj == "restartGame")
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
