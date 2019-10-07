7946b1f24a1ae68e317f5add5c1b2eaa29a0da4a
breynartstudios <breynartstudios@gmail.com>
2019-10-07 13:35:32 +0200

    Added another level and rotational spikes

---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    public bool rotate;
+    public bool clockWise;
+    public float rotationAmount = 25f;
+
+    void FixedUpdate()
+    {
+        if (rotate)
+        {
+            if(clockWise) {
+                transform.Rotate(0, 0, -rotationAmount * Time.deltaTime);
+            } else
+            {
+                transform.Rotate(0, 0, rotationAmount * Time.deltaTime);
+            }            
+        }
+    }
+}
